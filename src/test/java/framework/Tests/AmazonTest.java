d0a6c1a8c7e85eba6b3e905fcaf47621b656d499
zoetrigon <zoetriguerosgonzalez@gmail.com>
2022-02-12 09:57:17 +0100

    No funcional ni si quiera hace los primeros pasos

--- src/test/java/framework/Tests/AmazonTest.java
+++ src/test/java/framework/Tests/AmazonTest.java
@@ -5,35 +5,27 @@ import framework.Config.WebActions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
+import java.util.Scanner;
+
+//busqueda en wikipedia del enlace mas correcto preguntandole al usuario al inici
 
 public class AmazonTest extends WebActions{
-//modificado para usarlo con youtube
+//modificado para usarlo con wikipedia
     @Test(description = "Demo test")
     public void loginTest_Amazon() throws Exception {
         test = extent.createTest("Login Amazon.es");
 
         MainPage mainPage = new MainPage();
-        driver.get("https://www.youtube.com");
-        scrollDownTo(mainPage.Cookies_Youtube);
-        click(mainPage.Cookies_Youtube);
+        //pedimos datos por teclado
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese dato a buscar en la wikipedia");
+        String dato = scanner.nextLine();
+        //abrimos wikipedia
+        driver.get("https://es.wikipedia.org/wiki/Wikipedia:Portada");
+
 
         try {
-            waitForVisibility(mainPage.Login_Youtube);
-            click(mainPage.Login_Youtube);
-            //inicio de sesion
-            waitForVisibilitySendKeys(mainPage.Mail_youtube,datos.getProperty("user"));
-            click(mainPage.Siguiente_youtube);
-            waitForVisibilitySendKeys(mainPage.Password_youtube,datos.getProperty("password"));
-            waitForVisibility(mainPage.Siguiente2_youtube);
-            click(mainPage.Siguiente2_youtube);
-            waitForVisibilityClick(mainPage.Buscador_Youtube);
-            sendKeys(mainPage.Buscador_Youtube,datos.getProperty("videotitle"));
-            pressKey(mainPage.Buscador_Youtube, Keys.ENTER);
-            waitForVisibility(mainPage.Video_Youtube);
-            click(mainPage.Video_Youtube);
-            //poner video en pantalla completa
-            waitForVisibility(mainPage.Pantalla_Completa_Youtube);
-            click(mainPage.Pantalla_Completa_Youtube);
+
 
 
         } catch (Exception ex) {
